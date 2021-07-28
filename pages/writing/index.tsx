1bffc0ae296e72075371491720cff5298727447e
Agustin Burset <agustin.burset@icloud.com>
2021-07-28 14:49:03 -0300

    Show list of dummy articles on Writing, tweak styles

--- pages/writing/index.tsx
+++ pages/writing/index.tsx
@@ -1,11 +1,33 @@
+import Link from 'next/link'
+import { GetStaticProps } from 'next'
 import { Layout } from '@components/Layout'
 
-function WritingPage() {
+type Props = {
+  articleIds: Array<string>
+}
+
+function WritingPage(props: Props) {
   return (
     <Layout>
       <h1>Writing</h1>
+      <ul>
+        {props.articleIds.map((id) => (
+          <li key={id}>
+            <Link href={`/writing/${id}`}>
+              <a>{id}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </Layout>
   )
 }
 
+const getStaticProps: GetStaticProps<Props> = async () => {
+  return {
+    props: { articleIds: ['foo', 'bar'] },
+  }
+}
+
 export default WritingPage
+export { getStaticProps }
