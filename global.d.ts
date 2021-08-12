6efb30bb84f549a72f24f277c2ce4c45f265d58c
Agustin Burset <agustin.burset@icloud.com>
2021-08-12 14:17:05 -0300

    Setup prisma, define base database schema

---
+++ global.d.ts
@@ -0,0 +1,14 @@
+import type { PrismaClient } from '@prisma/client'
+
+// The ideal approach would be to just use 'declare namespace NodeJS { ... }'
+// (without using the 'global') and have access to the PrismaClient type via a
+// reference to '@prisma/client' (see the next-env.d.ts file for an example)
+// instead of using an import, but that specific reference doesn't seem to work.
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      prisma: PrismaClient
+    }
+  }
+}
