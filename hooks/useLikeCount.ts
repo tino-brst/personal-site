24a8925ec8888634cdce95f6e92a42e91e413b11
Agustin Burset <agustin.burset@icloud.com>
2021-08-24 15:38:50 -0300

    Handle max user likes on useLikeCount hook

--- hooks/useLikeCount.ts
+++ hooks/useLikeCount.ts
@@ -1,5 +1,6 @@
 import useSWR from 'swr'
 import { useDebounce } from './useDebounce'
+import { maxUserLikeCount } from 'lib/constants'
 
 function useLikeCount(slug: string) {
   const { data, error, mutate } = useSWR<{
@@ -10,20 +11,31 @@ function useLikeCount(slug: string) {
   const debouncedMutate = useDebounce(mutate, 1000)
 
   const increment = () => {
-    mutate(
-      (data) => ({
-        totalLikeCount: (data?.totalLikeCount ?? 0) + 1,
-        userLikeCount: (data?.userLikeCount ?? 0) + 1,
-      }),
-      false
-    )
+    // Update the local data immediately (allows the UI to reflect the change
+    // instantaneously). Revalidation is disabled to avoid an unnecessary fetch,
+    // which we'll handle next.
+    mutate((data) => {
+      if (data && data.userLikeCount < maxUserLikeCount) {
+        return {
+          totalLikeCount: (data.totalLikeCount ?? 0) + 1,
+          userLikeCount: (data.userLikeCount ?? 0) + 1,
+        }
+      }
+    }, false)
 
+    // Send the request (debounced) to update the user's like count in the
+    // source, and update the local copy with the returned (updated) value.
+    // Revalidation is disabled once again, since we're already getting the
+    // latest value from the POST request and returning it.
     debouncedMutate(async (data) => {
-      const response = await fetch(
-        `/api/likes/${slug}?count=${data?.userLikeCount ?? 0}`,
-        { method: 'POST' }
-      )
-      return await response.json()
+      if (data) {
+        const response = await fetch(
+          `/api/likes/${slug}?count=${data.userLikeCount}`,
+          { method: 'POST' }
+        )
+
+        return await response.json()
+      }
     }, false)
   }
 
