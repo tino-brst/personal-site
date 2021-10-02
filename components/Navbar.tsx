952938598b7c9c2b2ce273e58112a014a3c02d34
Agustin Burset <agustin.burset@icloud.com>
2021-10-02 16:42:39 -0300

    Add theme picker

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,15 @@
+import * as React from 'react'
 import Link from 'next/link'
+import { useTheme, ActiveTheme } from 'contexts/theme'
 
 function Navbar() {
+  const theme = useTheme()
+
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value as ActiveTheme
+    theme.setActive(value)
+  }
+
   return (
     <nav>
       <ul>
@@ -13,6 +22,16 @@ function Navbar() {
           <Link href="/writing">
             <a>Writing</a>
           </Link>
+        </li>
+        <li className="theme">
+          <select value={theme.active} onChange={handleChange}>
+            {theme.values.map((theme) => (
+              <option value={theme} onChange={console.log} key={theme}>
+                {theme}
+              </option>
+            ))}
+          </select>
+          <span className={'theme-resolved'}>({theme.resolved})</span>
         </li>
       </ul>
     </nav>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SWRConfig } from 'swr'
 import { fetcher } from '@lib/fetcher'
+import { ThemeProvider } from 'contexts/theme'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <SWRConfig value={{ fetcher }}>
-      <Component {...pageProps} />
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </SWRConfig>
   )
 }
