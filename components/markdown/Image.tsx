f42531a1e3bcff57534dddda12323784cde72d1a
Agustin Burset <agustin.burset@icloud.com>
2021-10-20 15:35:09 -0300

    Restore image spacing via image wrapper

--- components/markdown/Image.tsx
+++ components/markdown/Image.tsx
@@ -4,7 +4,11 @@ import NextImage from 'next/image'
 type Props = React.ComponentProps<typeof NextImage>
 
 function Image(props: Props) {
-  return <NextImage className="image" layout="responsive" {...props} />
+  return (
+    <div className="image-wrapper">
+      <NextImage className="image" layout="responsive" {...props} />
+    </div>
+  )
 }
 
 export { Image }
