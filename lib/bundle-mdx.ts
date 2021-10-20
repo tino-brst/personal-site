852ea0b5024b819defa5c2dcb8cb725ab1784eff
Agustin Burset <agustin.burset@icloud.com>
2021-10-20 15:34:38 -0300

    Remove wrapping p tags from markdown images
    Fixes "validateDOMNesting(...): <div> cannot appear as a descendant of <p> ..." errors

--- lib/bundle-mdx.ts
+++ lib/bundle-mdx.ts
@@ -3,11 +3,13 @@ import { rehypePrism } from './rehype-prism'
 import { rehypeImageSizes } from './rehype-image-sizes'
 import rehypeSlug from 'rehype-slug'
 import rehypeAutoLinkHeadings from 'rehype-autolink-headings'
+import remarkUnwrapImages from 'remark-unwrap-images'
 
 function customBundleMDX(mdxSource: string) {
   return bundleMDX(mdxSource, {
     xdmOptions: (options) => ({
       ...options,
+      remarkPlugins: [...(options.remarkPlugins ?? []), remarkUnwrapImages],
       rehypePlugins: [
         ...(options.rehypePlugins ?? []),
         rehypeImageSizes,
