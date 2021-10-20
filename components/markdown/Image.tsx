53ce54a077112f563ed2553befd32bc8eaff433d
Agustin Burset <agustin.burset@icloud.com>
2021-10-20 14:57:42 -0300

    Simplify Image component to just set a different set of default values

--- components/markdown/Image.tsx
+++ components/markdown/Image.tsx
@@ -1,27 +1,10 @@
 import * as React from 'react'
 import NextImage from 'next/image'
 
-type Props = {
-  alt: string
-  src: string
-}
-
-// TODO: make the aspect ratio configurable (not all images are going to have
-// the same shape). Maybe there is a remark plugin that exposes their size? And
-// that can be used as default
+type Props = React.ComponentProps<typeof NextImage>
 
 function Image(props: Props) {
-  return (
-    <div className="image-wrapper">
-      <NextImage
-        className="image"
-        src={props.src}
-        alt={props.alt}
-        objectFit="cover"
-        layout="fill"
-      />
-    </div>
-  )
+  return <NextImage className="image" layout="responsive" {...props} />
 }
 
 export { Image }
