a0a2a5897e27e74937186c37152ee7e5fd2a5b68
Agustin Burset <agustin.burset@icloud.com>
2021-10-22 10:07:25 -0300

    Simplify rehype-image-sizes plugin

--- lib/rehype-image-sizes.ts
+++ lib/rehype-image-sizes.ts
@@ -1,30 +1,20 @@
 import type { Plugin } from 'unified'
-import { Root, Element } from 'hast'
+import { Root } from 'hast'
 import { visit } from 'unist-util-visit'
-import { is } from 'unist-util-is'
 import getImageSize from 'image-size'
 
-type Options = Partial<{
-  /** Images root directory. Used to build the path for each image `(path = root + image.src`). */
-  root: string
-}>
-
-const rehypeImageSizes: Plugin<[Options?], Root> = (options) => {
-  return (tree) => {
-    visit(tree, (node) => {
-      if (
-        !is<Element>(node, { type: 'element', tagName: 'img' }) ||
-        !node.properties ||
-        typeof node.properties.src !== 'string'
-      ) {
+const rehypeImageSizes: Plugin<[], Root> = () => {
+  return (tree, file) => {
+    visit(tree, { type: 'element', tagName: 'img' }, (image) => {
+      if (!image.properties || typeof image.properties.src !== 'string') {
         return
       }
 
-      const imagePath = `${options?.root ?? ''}${node.properties.src}`
-      const imageSize = getImageSize(imagePath)
+      const imagePath = `${file.cwd}/public${image.properties.src}`
+      const { width, height } = getImageSize(imagePath)
 
-      node.properties.width = imageSize.width
-      node.properties.height = imageSize.height
+      image.properties.width = width
+      image.properties.height = height
     })
   }
 }
