678da07862ff25c92b82050e6de726163e42e31c
Agustin Burset <agustin.burset@icloud.com>
2022-03-15 13:41:55 -0300

    Implement useSize hook

---
+++ hooks/useSize.ts
@@ -0,0 +1,28 @@
+import * as React from 'react'
+
+type Size = {
+  width: number
+  height: number
+}
+
+function useSize<T extends HTMLElement>(): [React.RefObject<T>, Size] {
+  const ref = React.useRef<T>(null)
+  const [size, setSize] = React.useState<Size>({ width: 0, height: 0 })
+
+  React.useLayoutEffect(() => {
+    if (!ref.current) return
+
+    const resizeObserver = new ResizeObserver((entries) => {
+      const { width, height } = entries[0].contentRect
+      setSize({ width, height })
+    })
+
+    resizeObserver.observe(ref.current)
+
+    return () => resizeObserver.disconnect()
+  }, [])
+
+  return [ref, size]
+}
+
+export { useSize }
