533b1558201090ae7f2dc50dad2375af6ac821ac
Agustin Burset <agustin.burset@icloud.com>
2022-03-17 16:15:00 -0300

    Implement Stack & Spacer layout components

---
+++ components/Stack.tsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+const alignValue = {
+  start: 'flex-start',
+  center: 'center',
+  end: 'flex-end',
+}
+
+const Stack = styled('div').withConfig({
+  shouldForwardProp: (prop) => !['spacing'].includes(prop),
+})<{
+  spacing?: number
+  align?: keyof typeof alignValue
+}>`
+  display: flex;
+  gap: ${({ spacing }) => (spacing ? `${spacing}px` : null)};
+  align-items: ${({ align }) => (align ? alignValue[align] : null)};
+`
+
+const HStack = styled(Stack)``
+
+const VStack = styled(Stack)`
+  flex-direction: column;
+`
+
+export { HStack, VStack }
