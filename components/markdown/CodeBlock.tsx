8447be90496c30c325c137a8a3bcd953efe77f49
Agustin Burset <agustin.burset@icloud.com>
2022-03-28 12:43:47 -0300

    Update CodeBlock component, remove related unused styles

--- components/markdown/CodeBlock.tsx
+++ components/markdown/CodeBlock.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import styled from 'styled-components'
 
 type Props = {
   children?: React.ReactNode
@@ -13,11 +14,42 @@ function CodeBlock(props: Props) {
   }
 
   return (
-    <div>
-      <pre {...props} ref={preElementRef} />
-      <button onClick={handleClick}>Copy</button>
-    </div>
+    <Pre {...props} ref={preElementRef}>
+      {props.children}
+      {/* <CopyButton onClick={handleClick}>Copy</CopyButton> */}
+    </Pre>
   )
 }
+
+const Pre = styled.pre`
+  position: relative;
+  line-height: 1.4;
+  background-color: hsla(0 0% 0% / 0.03);
+  margin-left: -24px;
+  margin-right: -24px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+
+  & .line {
+    padding-left: 24px;
+    padding-right: 24px;
+  }
+
+  & .line.highlight {
+    background-color: hsla(0 0% 0% / 0.04);
+  }
+
+  @media (min-width: 768px) {
+    border-radius: 10px;
+  }
+`
+
+const CopyButton = styled.div`
+  position: absolute;
+  top: 24px;
+  right: 24px;
+`
 
 export { CodeBlock }
