6cc7e234c57cc33d2452e8c977f6c070b3c6c626
Agustin Burset <agustin.burset@icloud.com>
2022-04-18 07:33:22 -0300

    Base layout, NavBar style tweaks

--- pages/writing/index.tsx
+++ pages/writing/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import styled from 'styled-components'
 import Link from 'next/link'
 import { GetStaticProps } from 'next'
 import { useRouter } from 'next/dist/client/router'
@@ -94,10 +95,51 @@ function WritingPage(props: Props) {
     })
   }
 
+  const article = articles[0]
+
   return (
     <Layout>
-      <h1>Writing</h1>
-      <div className="filters">
+      <Wrapper>
+        <Title>Writing</Title>
+        <List>
+          <ListItem>
+            <ItemImage />
+            <ItemDescription>
+              Id commodo magna irure sint ea sunt occaecat sunt qui. Do esse
+              Lorem culpa mollit ad non incididunt ut ullamco cupidatat non
+              anim.
+            </ItemDescription>
+          </ListItem>
+          <ListItem>
+            <ItemImage />
+            <ItemDescription>
+              Laboris sit reprehenderit aliqua officia minim mollit deserunt
+              deserunt. Voluptate eu deserunt consectetur amet aliqua. Ex qui
+              anim sunt culpa nulla magna excepteur sit est aliqua esse cillum.
+              Ad qui ex sit incididunt sit commodo ex aute aliqua amet.
+              Voluptate irure voluptate ex ullamco ad consectetur adipisicing
+              velit ea.
+            </ItemDescription>
+          </ListItem>
+          <ListItem>
+            <ItemImage />
+            <ItemDescription>
+              Id commodo magna irure sint ea sunt occaecat sunt qui. Do esse
+              Lorem culpa mollit ad non incididunt ut ullamco cupidatat non
+              anim.
+            </ItemDescription>
+          </ListItem>
+          <ListItem>
+            <ItemImage />
+            <ItemDescription>
+              Id commodo magna irure sint ea sunt occaecat sunt qui. Do esse
+              Lorem culpa mollit ad non incididunt ut ullamco cupidatat non
+              anim.
+            </ItemDescription>
+          </ListItem>
+        </List>
+
+        {/* <div className="filters">
         <div className="filters__search">
           <input
             type="text"
@@ -121,33 +163,104 @@ function WritingPage(props: Props) {
             </label>
           ))}
         </div>
-      </div>
-      <ul className="articles-list">
-        {articles.map((article) => (
-          <li key={article.slug}>
-            <Link href={article.url}>
-              <a>
-                {article.thumbnailImageSrc && (
-                  <ThumbnailImage src={article.thumbnailImageSrc} />
-                )}
-                <h3 dangerouslySetInnerHTML={{ __html: article.title }} />
-              </a>
-            </Link>
-            <span>
-              {article.readingTime} • {formatDate(article.publishedOn)}
-            </span>
-          </li>
-        ))}
-      </ul>
-      {/* TODO: define proper empty states ("no articles found with tags #Foo and #Bar") */}
-      {!articles.length && (
-        <p>
-          <strong>No articles found</strong>
-        </p>
-      )}
+      </div> */}
+        {/* <ul className="articles-list">
+          {articles.map((article) => (
+            <li key={article.slug}>
+              <Link href={article.url}>
+                <a>
+                  {article.thumbnailImageSrc && (
+                    <ThumbnailImage src={article.thumbnailImageSrc} />
+                  )}
+                  <h3 dangerouslySetInnerHTML={{ __html: article.title }} />
+                </a>
+              </Link>
+              <span>
+                {article.readingTime} • {formatDate(article.publishedOn)}
+              </span>
+            </li>
+          ))}
+        </ul> */}
+        {/* TODO: define proper empty states ("no articles found with tags #Foo and #Bar") */}
+        {!articles.length && (
+          <p>
+            <strong>No articles found</strong>
+          </p>
+        )}
+      </Wrapper>
     </Layout>
   )
 }
+
+const Wrapper = styled.div`
+  max-width: 768px;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 24px;
+  padding-right: 24px;
+`
+
+// TODO: extract to component shared across pages?
+const Title = styled.h1`
+  font-size: 2.2rem;
+  font-weight: 600;
+  margin-top: 18px;
+  margin-bottom: 20px;
+`
+
+const List = styled.ol`
+  margin-left: -8px;
+  margin-right: -8px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+
+  @media (min-width: 640px) {
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+`
+
+const ListItem = styled.li`
+  flex: 0 1 calc(50% - 16px / 2);
+  padding: 8px;
+  background-color: hsla(40deg 100% 95% / 1);
+
+  display: flex;
+  flex-direction: column;
+
+  @media (min-width: 640px) {
+    &:first-child {
+      flex-basis: 100%;
+      flex-direction: row;
+    }
+  }
+`
+
+const ItemImage = styled.div`
+  aspect-ratio: 2 / 1;
+  background-color: hsla(240deg 100% 50% / 0.1);
+
+  @media (min-width: 640px) {
+    ${ListItem}:first-child & {
+      flex: 2 1 0;
+    }
+  }
+`
+
+const ItemDescription = styled.div`
+  flex-grow: 1;
+  background-color: hsla(120deg 100% 50% / 0.1);
+
+  padding: 8px;
+
+  @media (min-width: 640px) {
+    ${ListItem}:first-child & {
+      flex: 1 1 0;
+    }
+  }
+`
 
 /**
  * Given:
