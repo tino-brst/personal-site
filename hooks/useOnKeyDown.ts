ebf617fb95b3efdc6ef5e4e668a93823827f0f1d
Agustin Burset <agustin.burset@icloud.com>
2022-05-12 14:26:10 -0300

    Add useOnKeyDown custom hook

---
+++ hooks/useOnKeyDown.ts
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import { useWindowEventListener } from './useWindowEventListener'
+
+function useOnKeyDown(
+  /** Key to run the callback function on (same values as KeyboardEvent.key) */
+  key: string,
+  fn: (event: KeyboardEvent) => void,
+  isEnabled = true
+) {
+  const keyDownHandler = React.useCallback(
+    (event: KeyboardEvent) => {
+      if (event.key === key) {
+        fn(event)
+      }
+    },
+    [key, fn]
+  )
+
+  useWindowEventListener('keydown', keyDownHandler, isEnabled)
+}
+
+export { useOnKeyDown }
