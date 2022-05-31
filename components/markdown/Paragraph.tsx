d3c7fc6da16f890849523399f89d40ef3fcfb6a5
Agustin Burset <agustin.burset@icloud.com>
2022-05-31 15:00:06 -0300

    Add caps first letter ala magazine

--- components/markdown/Paragraph.tsx
+++ components/markdown/Paragraph.tsx
@@ -1,9 +1,24 @@
 import styled from 'styled-components'
 
 const Paragraph = styled.p`
-  line-height: 1.5;
+  line-height: 1.5em;
   margin-top: 20px;
   margin-bottom: 20px;
+
+  main > &:first-of-type {
+    /* Takes into account */
+    min-height: 2.9em;
+  }
+
+  main > &:first-of-type::first-letter {
+    font-size: 2.9em;
+    font-weight: 500;
+    float: left;
+    line-height: 0.9em;
+    margin-right: 8px;
+    margin-top: 5px;
+    color: black;
+  }
 `
 
 export { Paragraph }
