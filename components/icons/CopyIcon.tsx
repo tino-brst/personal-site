fc76df160bc17b0e7e2ea9cca46206b228d836f5
Agustin Burset <agustin.burset@icloud.com>
2022-07-06 15:07:59 -0300

    Add copy/checkmark icons

---
+++ components/icons/CopyIcon.tsx
@@ -0,0 +1,76 @@
+import styled from 'styled-components'
+
+type Props = {
+  className?: string
+}
+
+function CopyIcon(props: Props) {
+  return (
+    <Root
+      className={props.className}
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <defs>
+        <mask id="copy-icon-mask">
+          <rect width="100%" height="100%" fill="black" />
+          <rect
+            className="rect-back"
+            x="2"
+            y="2"
+            width="11"
+            height="11"
+            rx="2"
+          />
+          <rect
+            className="rect-background"
+            x="7"
+            y="7"
+            width="11"
+            height="11"
+            rx="2"
+          />
+          <rect
+            className="rect-front"
+            x="7"
+            y="7"
+            width="11"
+            height="11"
+            rx="2"
+          />
+        </mask>
+      </defs>
+      <rect width="100%" height="100%" mask="url(#copy-icon-mask)" />
+    </Root>
+  )
+}
+
+const Root = styled.svg`
+  color: black;
+  stroke-width: 0;
+  fill: currentColor;
+
+  mask {
+    fill: none;
+    stroke: white;
+  }
+
+  .rect-back {
+    fill: none;
+    stroke-width: 1.5;
+  }
+
+  .rect-front {
+    fill: black;
+    stroke-width: 1.5;
+  }
+
+  .rect-background {
+    stroke-width: 4px;
+    stroke: black;
+  }
+`
+
+export { CopyIcon }
