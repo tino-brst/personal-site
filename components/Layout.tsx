f1b95b686b3a58f8074fa9c27b214449f40f727b
Agustin Burset <agustin.burset@icloud.com>
2022-08-24 15:32:22 -0300

    Extract Footer

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,21 +1,13 @@
-import { useIsInView } from '@hooks/useIsInView'
 import { useIsomorphicLayoutEffect } from '@hooks/useIsomorphicLayoutEffect'
-import clsx from 'clsx'
 import { useNavBar } from 'contexts/nav-bar'
-import NextLink from 'next/link'
 import { useRouter } from 'next/router'
 import * as React from 'react'
 import styled, { css } from 'styled-components'
 import { focusRing } from 'styles/focusRing'
-import { ArrowTopRightIcon } from './icons/ArrowTopRightIcon'
+import { Footer } from './Footer'
 import { NavBar } from './NavBar'
-import { Signature } from './Signature'
 
-type Props = {
-  children?: React.ReactNode
-}
-
-function Layout(props: Props) {
+function Layout(props: React.PropsWithChildren<{}>) {
   const router = useRouter()
   const navBar = useNavBar()
 
@@ -41,35 +33,11 @@ function Layout(props: Props) {
     }
   }, [router.events])
 
-  // Footer entrance animation
-  // TODO extract Footer
-
-  const footerRef = React.useRef(null)
-  const isFooterInView = useIsInView(footerRef, {
-    once: true,
-    viewMargins: '-20px',
-  })
-
   return (
     <Wrapper>
       <NavBar />
       <Content>{props.children}</Content>
-      <Footer ref={footerRef} className={clsx({ show: isFooterInView })}>
-        <LinksWrapper>
-          <Link href="https://linkedin.com/in/agustin-burset/" external>
-            LinkedIn
-          </Link>
-          <Link href="https://twitter.com/bursetAgustin" external>
-            Twitter
-          </Link>
-          <Link href="https://github.com/tino-brst" external>
-            GitHub
-          </Link>
-          <Link href="mailto:tinos.corner@icloud.com">Email</Link>
-        </LinksWrapper>
-        <SignatureLabel>made with care by</SignatureLabel>
-        <Signature />
-      </Footer>
+      <Footer />
       <PS>
         <p>
           p.s. the site&apos;s code is open source and{' '}
@@ -79,23 +47,6 @@ function Layout(props: Props) {
         </p>
       </PS>
     </Wrapper>
-  )
-}
-
-type LinkProps = {
-  href: string
-  external?: boolean
-  children?: React.ReactNode
-}
-
-function Link(props: LinkProps) {
-  return (
-    <NextLink href={props.href} passHref>
-      <StyledLink target={props.external ? '_blank' : undefined}>
-        {props.children}
-        {props.external && <ExternalLinkIcon />}
-      </StyledLink>
-    </NextLink>
   )
 }
 
@@ -112,133 +63,6 @@ const Wrapper = styled.div`
 const Content = styled.div`
   flex: 1;
   padding-top: ${NavBar.marginBottom}px;
-`
-
-const Footer = styled.footer`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  position: relative;
-  opacity: 0;
-  transform: translateY(20px);
-
-  transition-property: opacity, transform;
-  transition-duration: 0.6s;
-
-  max-width: calc(768px + 2 * 16px);
-  margin-top: 48px;
-  margin-left: auto;
-  margin-right: auto;
-  padding-left: 24px;
-  padding-right: 24px;
-  padding-top: 48px;
-  padding-bottom: 44px;
-
-  &::before {
-    position: absolute;
-    top: 0;
-    left: 16px;
-    right: 16px;
-    display: block;
-    content: '';
-    height: 1px;
-    background-color: var(--color-border);
-    transform: scaleX(0);
-
-    transition-property: transform;
-    transition-duration: 0.6s;
-    transition-delay: 0.2s;
-  }
-
-  &.show {
-    opacity: 1;
-    transform: none;
-  }
-
-  &.show::before {
-    transform: none;
-  }
-
-  @media (min-width: 640px) {
-    padding-left: 40px;
-    padding-right: 40px;
-
-    &::before {
-      left: 32px;
-      right: 32px;
-    }
-  }
-`
-
-const LinksWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 18px;
-  justify-content: center;
-  margin-bottom: 48px;
-`
-
-const styledLinkHoverStyles = css`
-  color: var(--color-fg-default-hover);
-`
-
-const StyledLink = styled.a`
-  position: relative;
-  display: flex;
-  gap: 4px;
-  font-size: 14px;
-  font-weight: 450;
-  line-height: 1;
-  color: var(--color-fg-default);
-
-  transition-property: color;
-  transition-duration: 0.1s;
-
-  @media (hover: hover) {
-    &:hover {
-      ${styledLinkHoverStyles}
-    }
-  }
-
-  &:active,
-  &:focus-visible {
-    ${styledLinkHoverStyles}
-  }
-
-  --focus-inset: -6px;
-  --focus-radius: 8px;
-
-  ${focusRing}
-`
-const externalLinkIconHoverStyles = css`
-  color: var(--color-fg-muted-hover);
-`
-
-const ExternalLinkIcon = styled(ArrowTopRightIcon)`
-  color: var(--color-fg-muted);
-  margin-right: -1px;
-
-  transition-property: color;
-  transition-duration: 0.1s;
-
-  ${StyledLink}:active &, 
-  ${StyledLink}:focus-visible & {
-    ${externalLinkIconHoverStyles}
-  }
-
-  @media (hover: hover) {
-    ${StyledLink}:hover & {
-      ${externalLinkIconHoverStyles}
-    }
-  }
-`
-
-const SignatureLabel = styled.label`
-  font-size: 12px;
-  color: var(--color-fg-muted);
-  font-weight: 500;
-  margin-bottom: 10px;
 `
 
 const psLinkHoverStyles = css`
