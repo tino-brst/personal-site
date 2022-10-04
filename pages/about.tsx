cec2fe04bb67c445560e605a26b61dc382076338
Agustin Burset <agustin.burset@icloud.com>
2022-10-04 16:59:42 -0300

    Update About page images

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,22 +1,34 @@
 import { Link } from '@components/Link'
-import { Parallax } from '@components/Parallax'
 import { getStaggerProps } from '@lib/stagger'
 import NextImage from 'next/image'
 import styled from 'styled-components'
-import aboutProfileImageSrc from '/public/images/tino.jpg'
+import tinoPointingImageSrc from '/public/images/tino-pointing.jpg'
+import tinoImageSrc from '/public/images/tino.jpg'
 
 function AboutPage() {
   // BLKD SEO
-  // TODO Remove parallax
 
   return (
     <Root>
       <Title {...getStaggerProps(0)}>About me</Title>
-      <ImageMask {...getStaggerProps(1)}>
-        <StyledParallax multiplier={-0.025} clampTo={10}>
-          <NextImage src={aboutProfileImageSrc} placeholder="blur" />
-        </StyledParallax>
-      </ImageMask>
+      <CardsContainer {...getStaggerProps(1)}>
+        <CardBackground>
+          <NextImage
+            src={tinoPointingImageSrc}
+            layout="fill"
+            objectFit="cover"
+            placeholder="blur"
+          />
+        </CardBackground>
+        <CardForeground>
+          <NextImage
+            src={tinoImageSrc}
+            layout="fill"
+            objectFit="cover"
+            placeholder="blur"
+          />
+        </CardForeground>
+      </CardsContainer>
       <Description {...getStaggerProps(2)}>
         <p>
           Dolor irure qui ex nisi sit eiusmod enim ipsum reprehenderit ex
@@ -46,9 +58,13 @@ const Root = styled.div`
   padding-left: 24px;
   padding-right: 24px;
 
+  /* Prevents horizontal scroll due to translate transform on background pic */
+  overflow: hidden;
+
   @media (min-width: 640px) {
     padding-left: 40px;
     padding-right: 40px;
+    overflow: unset;
   }
 `
 
@@ -57,33 +73,55 @@ const Title = styled.h1`
   font-size: 2.2rem;
   font-weight: 600;
   margin-top: 18px;
-  margin-bottom: 24px;
+  margin-bottom: 30px;
 `
 
-// TODO revise contrasting border (maybe make it light like in the articles
-// grid?)
-const ImageMask = styled.div`
-  border-radius: 12px;
-  overflow: hidden;
-  margin-bottom: 24px;
-  aspect-ratio: 5 / 4;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: var(--color-bg-subtlerer);
-  box-shadow: inset 0 0 0 1px var(--color-shadow-border),
-    0 0 0 1px var(--color-shadow-border-contrast);
+const CardsContainer = styled.div`
+  aspect-ratio: 4 / 3;
+  display: grid;
+  grid-template-columns: 24px 50% auto;
+  grid-template-rows: 40% 45% auto;
+  margin-left: -24px;
+  margin-right: -24px;
 
   @media (min-width: 640px) {
-    aspect-ratio: 5 / 3;
+    aspect-ratio: 9 / 6;
+    margin-right: 0;
   }
 `
 
-const StyledParallax = styled(Parallax)`
+const Card = styled.div`
+  --border-radius: 10px;
+
   position: relative;
-  z-index: -1;
-  margin-top: -10px;
-  margin-bottom: -10px;
+  border-radius: var(--border-radius);
+  overflow: hidden;
+
+  ::after {
+    position: absolute;
+    border-radius: var(--border-radius);
+    content: '';
+    inset: 0;
+    pointer-events: none;
+    box-shadow: inset 0 0 0 1px hsla(0 0% 100% / 0.2),
+      inset 0 0 0 0.5px hsla(0 0% 0% / 0.2);
+  }
+`
+
+const CardBackground = styled(Card)`
+  grid-column: 2 / -1;
+  grid-row: 1 / 3;
+
+  transform: rotate(1deg) translateX(2px);
+  box-shadow: 0 0 10px 0 hsla(0 0% 0% / 0.08);
+`
+
+const CardForeground = styled(Card)`
+  grid-column: 1 / 3;
+  grid-row: 2 / -1;
+
+  transform: rotate(-1deg) translateX(-2px);
+  box-shadow: 0 0 30px 0 hsla(0 0% 0% / 0.2);
 `
 
 const Description = styled.div`
@@ -91,7 +129,7 @@ const Description = styled.div`
     font-size: 16px;
     color: var(--color-fg-default);
     line-height: 1.5;
-    margin-top: 20px;
+    margin-top: 28px;
     margin-bottom: 20px;
   }
 `
